eff380e6a6606274662d6ab59e5dce9dcfcc9d0b
krishnathakkar29 <work.krishnat@gmail.com>
2024-05-14 19:30:59 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+//react split 
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button size={"sm"} variant={"destructive"}>Button</Button>
+
+    </>
+  )
+}
+
+export default App
